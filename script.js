8ccac77edb81eae2d107aaadb95870dab02b28bc
AlexandraMomoka <tishenkoaleksandra2002@gmail.com>
2021-12-28 22:43:40 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+  
+delay(1000).then(() => console.log('Hello!'));
+
+function getUserInfo() {
+    return new Promise(function(resolve, reject) {
+        let userData = { name: 'Vic', age: 21, id: 1 };
+        setTimeout(() => resolve(userData), 1000);
+    });
+}
+
+function getUserAvatar(userInfo) {
+    return new Promise(function(resolve, reject) {
+        userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg'
+        setTimeout(() => resolve(userInfo), 1000);
+    });
+}
+
+function getUserAdditionalInfo(userInfo) {
+    return new Promise(function(resolve, reject) {
+        userInfo.interests = ['sport', 'books'];
+        setTimeout(() => resolve(userInfo), 1000);
+    });
+}
+
+
+getUserInfo()
+    .then((userData) => { return userData})
+    .then((userInfo) => {
+        return  getUserAvatar(userInfo)
+    })
+    .then((userInfo) => {
+        return getUserAdditionalInfo(userInfo)
+    })
+    .then((userData) => {console.log(userData)})
+
+
+let promise = new Promise(function(resolve, reject) {
+    setTimeout(() => resolve({ name: 'Vic', age: 21, id: 1 } ), 1000);
+    })
+    .then(function(userInfo) {
+      return new Promise(function(resolve, reject) {
+        setTimeout(() => reject(new Error('wrong data') ), 1000);
+    });
+})
+
+promise
+    .catch((userInfo) => console.error(userInfo))
